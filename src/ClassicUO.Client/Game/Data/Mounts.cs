cbe62dda862b1759b357379a4b6b9928b793b360
TazmanianTad <bittiez@gmail.com>
2025-10-22 19:47:09 -0600

    - Add offset for manticore

--- src/ClassicUO.Client/Game/Data/Mounts.cs
+++ src/ClassicUO.Client/Game/Data/Mounts.cs
@@ -75,7 +75,7 @@ internal static class Mounts
         _mounts[0x3EDA] = new(0x0590, 0x3EDA, 9); // Frostmites Beetles
         _mounts[0x3EDE] = new(0x0673, 0x3EDE, 0); // Clydesdale?
         _mounts[0x3EDC] = new(0x0666, 0x3EDC, 0); // Moldering Ursine?
-        _mounts[0x3EDB] = new(0x0611, 0x3EDB, 0); // Manticore
+        _mounts[0x3EDB] = new(0x0611, 0x3EDB, 9); // Manticore
         _mounts[0x3EDD] = new(0x0581, 0x3EDD, 0); // Dragon_Hildebrandt
         _mounts[0x3EDF] = new(0x0676, 0x3EDF, 0); // Horse_Elemental_Earth
         _mounts[0x3EE0] = new(0x0677, 0x3EE0, 0); // Horse_Elemental_Fire
